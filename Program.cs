54e1bfc5107fc2df578edcf6637bf99d6a03cc47
CHEE LONG KHOO <khoo@dxc.com>
2022-03-07 16:17:00 +0800

    This C# program will reverse whole sentence input by user.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Reverse_string
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Here is a program to reverse your input, please enter a input:");
+            string input = Console.ReadLine();
+
+            string output = ReverseString(input);
+            
+            Console.WriteLine(output);
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] results = s.ToCharArray();
+
+            Array.Reverse(results);
+
+            return new string(results);
+        }
+
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Reverse_string
 This C# program will reverse whole sentence input by user.
+
+### This C# prgram can reverse whole sentence input by user such as scenario below:
+- If user input "Hello world", then it will retunr "world Hello".
